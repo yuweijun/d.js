b3163378f171d6122ed7b9e3e873fb26f26db142
yuweijun <yuweijun@live.com>
2018-11-05 11:08:26 +0800

    update tools.js

--- tools.js
+++ tools.js
@@ -2,8 +2,8 @@
 
     var stack = new document.$.Stack;
 
-    var outlined = (function* () {
-        while(true) {
+    var outlined = (function*() {
+        while (true) {
             document.$("*").forEach(function(elem) {
                 elem.style.outline = "1px solid #" + (~~(Math.random() * (1 << 24))).toString(16);
             });
@@ -18,30 +18,32 @@
         return encodeURIComponent(s).toLowerCase();
     };
 
-    var youdao = function() {
-        let s = document.$.selection();
-        open('http://dict.youdao.com/w/' + encode(s)).focus();
-    };
-
-    var dict = function() {
-        let s = document.$.selection();
-        open('http://dict.cn/' + encode(s)).focus();
-    };
-
-    var translate = function() {
-        let s = document.$.selection();
-        open('https://translate.google.com/#en/zh-CN/' + encode(s)).focus();
-    };
-
     document.addEventListener('keydown', function(e) {
         if (document.querySelectorAll('input:focus, textarea:focus, select:focus').length) return;
 
         let k = e.key;
         stack.push(k);
+
         stack.match(k, 'bb', outlined.next.bind(outlined));
-        stack.match(k, 'tt', translate);
-        stack.match(k, 'yt', youdao);
-        stack.match(k, 'dt', dict);
+
+        stack.match(k, 'tt', function() {
+            let s = document.$.selection();
+            open('https://translate.google.com/#en/zh-CN/' + encode(s)).focus();
+        });
+
+        stack.match(k, 'yt', function() {
+            let s = document.$.selection();
+            open('http://dict.youdao.com/w/' + encode(s)).focus();
+        });
+
+        stack.match(k, 'dt', function() {
+            let s = document.$.selection();
+            open('http://dict.cn/' + encode(s)).focus();
+        });
+
+        stack.match(k, 'ev', function() {
+            open('https://app.yinxiang.com/Home.action').focus();
+        });
 
     });
 
