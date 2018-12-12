abc21e93cd53a3af6cddd4c8c0130f0abe42dc96
yuweijun <yuweijun@live.com>
2018-12-12 15:16:00 +0800

    update article.js for readable

--- article.js
+++ article.js
@@ -1,7 +1,7 @@
 (function() {
 
     document.head.insertAdjacentHTML('beforeend', `<style id="tampermonkey-outline-style">
-        .target-outline { outline: 2px dashed rgba(255, 0, 0, .4); }
+        .target-outline { outline: 1px dashed rgba(202, 96, 133, .5); }
     </style>`);
 
     var stack = new document.$.Stack(4, 1000);
@@ -55,6 +55,52 @@
                 target.remove();
             }
         });
+    });
+
+    Object.defineProperty(NodeList.prototype, 'readable', {
+        value() {
+            if (this.length === 0) return this;
+
+            var parents = document.$(this).parents();
+
+            this.forEach(function(elem) {
+                while (elem && elem.tagName.toUpperCase() !== 'BODY') {
+                    var $elem = document.$(elem);
+                    $elem.css({
+                        padding: 0,
+                        margin: '0 auto',
+                        display: 'block',
+                        width: '100%',
+                        maxWidth: '1024px',
+                        position: 'static'
+                    });
+
+                    $elem.siblings().filter(function(elem) {
+                        if (elem.tagName.toUpperCase() === 'LINK') {
+                            return false;
+                        }
+                        if (parents.indexOf(elem) > -1) {
+                            return false;
+                        }
+                        return true;
+                    }).remove();
+
+                    elem = elem.parentNode;
+                }
+            });
+
+            document.$(this).css({
+                padding: '15px',
+                boxSizing: 'border-box'
+            });
+
+            document.$('head script').remove();
+            document.$('head').append(document.body.querySelectorAll('link'));
+            document.body.style.height = document.documentElement.scrollHeight + 'px';
+
+            return this;
+        },
+        enumerable: false
     });
 
 })();
--- d.js
+++ d.js
@@ -390,51 +390,6 @@
             },
             enumerable: false
         };
-        attr.readable = {
-            value() {
-                if (this.length === 0) return this;
-
-                var parents = this.parents();
-
-                this.forEach(function(elem) {
-                    while (elem && elem.tagName.toUpperCase() !== 'BODY') {
-                        var $elem = $(elem);
-                        $elem.css({
-                            padding: 0,
-                            margin: '0 auto',
-                            display: 'block',
-                            width: '100%',
-                            maxWidth: '1024px',
-                            position: 'static'
-                        });
-
-                        $elem.siblings().filter(function(elem) {
-                            if (elem.tagName.toUpperCase() === 'LINK') {
-                                return false;
-                            }
-                            if (parents.indexOf(elem) > -1) {
-                                return false;
-                            }
-                            return true;
-                        }).remove();
-
-                        elem = elem.parentNode;
-                    }
-                });
-
-                $(this).css({
-                    padding: '15px',
-                    boxSizing: 'border-box'
-                });
-
-                document.$('head script').remove();
-                $(document.head).append(document.body.querySelectorAll('link'));
-                document.body.style.height = document.documentElement.scrollHeight + 'px';
-
-                return this;
-            },
-            enumerable: false
-        };
 
         return Object.create(array, attr);
     };
