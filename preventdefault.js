791817d4db5930667092ed1fe6a543d8038e5760
Weijun Yu <weijun.yu@sap.com>
2020-04-16 22:12:59 +0800

    update preventdefault.js

--- preventdefault.js
+++ preventdefault.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         click.prevent.default
+// @name         prevent.default
 // @namespace    http://tampermonkey.net/
-// @version      2020.04.18
+// @version      2018.11.06
 // @author       test.yu
 // @match        http*://*/*
 // @grant        none
@@ -14,7 +14,7 @@
     };
 
     let prevent = false;
-    document.$.Stack.register('cc', function() {
+    let action = function() {
         if (!prevent) {
             prevent = true;
             document.querySelectorAll('a').forEach(function(elem) {
@@ -26,5 +26,11 @@
                 elem.removeEventListener('click', fn, false);
             });
         }
+    };
+
+    document.$.Stack.register('cc', action);
+
+    document.addEventListener('keydown', function(e) {
+        if (e.which === 27) action();
     });
 })();
